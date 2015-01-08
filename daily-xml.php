594a3b4798feff37f4c497ac7f4abc165386b31c
Andrew Stephens <boss@andrewanimation.biz>
2015-01-08 13:52:40 -0500

    Adding Download Button to Post Listing

--- daily-xml.php
+++ daily-xml.php
@@ -8,25 +8,32 @@ Author URI: http://andrewmediaprod.com/
 Version: 3.0
 */
 
-/* Adds a box to the main column on the Post and Page edit screens */
+// Create Meta Box for Post Edit Screens
 add_action('admin_init', 'tdaily_export_html_box', 1);
 function tdaily_export_html_box() {
 	add_meta_box( 'tdaily_export_html', 'InDesign Options', 'tdaily_export_html_box_inner', 'post', 'side','high' );
 }
 
-/* Prints the box content */
+// Box Content
 function tdaily_export_html_box_inner($post, $metabox) {
 ?>
 
 <div class="inside" style="margin:0;padding:6px 0">
-
 	<a class="button" href="<?php bloginfo('url'); ?>/?p=<?php echo $post->ID; ?>&amp;preview_id=<?php echo $post->ID; ?>&amp;InDesign=download" style="margin-right: 10px;">Download XML</a>
-
 </div>
 
 <?php
 }
 
+// Add XML Download Action to Post List Screen
+$xml_callback = function($actions) {
+	global $post;
+	$actions[] = '<a href="'.get_bloginfo('url').'/?p='.$post->ID.'&amp;preview_id='.$post->ID.'&amp;InDesign=download" title="Download XML for InDesign" target="_blank">XML</a>';
+	return $actions;
+};
+add_filter('post_row_actions', $xml_callback);
+
+// Call the XML Generator when Loaded
 add_action('single_template', 'tdaily_export_html');
 function tdaily_export_html($single_template) {
 	if (isSet($_GET['InDesign']) && $_GET['InDesign'] == 'download') {
